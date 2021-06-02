f6cae639f44dc0582277848b67cf068b35cf576c
Stenio Oliveira <steniooliv@gmail.com>
2021-06-01 23:32:43 -0300

    create transaction table component

---
+++ src/components/TransactionTable/index.tsx
@@ -0,0 +1,35 @@
+import { Container } from "./styles";
+
+export function TransactionTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>01/06/2021</td>
+          </tr>
+
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">-R$1.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>01/06/2021</td>
+          </tr>
+
+        </tbody>
+      </table>
+    </Container>
+  );
+}
