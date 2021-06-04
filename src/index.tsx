cb8aa55b5313c26fd571e5af1ae067c20043ad8f
Stenio Oliveira <steniooliv@gmail.com>
2021-06-03 23:20:49 -0300

    create seeds on miragejs db

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id:1,
+          title: "Freelance de website",
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-06-03 23:00'),
+        },
+        {
+          id:2,
+          title: "Aluguel",
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1200,
+          createdAt: new Date('2021-06-04 08:00'),
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
     
